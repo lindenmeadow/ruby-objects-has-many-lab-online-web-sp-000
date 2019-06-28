5bb8e73c488393bb0827100735038901a7045e2d
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-06-28 16:55:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,25 +2,25 @@ class Artist
   attr_accessor :name
 
   @@song_count = 0
-  
+
   def initialize(name)
     @name = name
     @songs = []
   end
-  
+
   def add_song(song)
     @songs << song
     song.artist = self
     @@song_count += 1
   end
-  
+
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
     @@song_count += 1
   end
-  
+
   def songs
     @songs
   end

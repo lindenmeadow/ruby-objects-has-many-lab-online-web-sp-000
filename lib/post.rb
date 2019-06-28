76683c62d97d28c733f3117c6dc0fc0dd35feec7
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-06-28 17:01:33 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,3 +1,21 @@
 class Post
+  class Song
+    attr_accessor :name, :author
 
+    @@all = []
+
+    def initialize(name, author = nil)
+      @name = name
+      @author = author
+      @@all << self
+    end
+
+    def author_name
+      if self.author
+        self.author.name
+      else
+        nil
+      end
+    end
+  end
 end
